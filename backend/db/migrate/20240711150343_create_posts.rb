c18222be56ab68bd67d18119180b8c2f9bbc20be
seus31 <us.balut.big.dipper@gmail.com>
2024-07-12 23:18:39 +0900

    add: マイグレーションの追加

--- backend/db/migrate/20240711150343_create_posts.rb
+++ backend/db/migrate/20240711150343_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[7.1]
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.column :status, "enum('not_publish', 'published')"
+      t.integer :status
       t.timestamps
     end
   end
