767babdfca7ad20fe75c42b36d47b2c98e58a0ba
seus31 <us.balut.big.dipper@gmail.com>
2024-07-18 10:01:45 +0900

    add: 投稿作成の処理追加

--- backend/app/controllers/api/v1/posts_controller.rb
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -1,9 +1,28 @@
 module Api
   module V1
-    class PostsController < ApplicationController
+    class PostsController < BaseController
       def index
         posts = Post.all
         render json: posts, each_serializer: PostSerializer
+      end
+
+      def create
+        post = Post.new(post_params)
+        if post.save
+          render json: post, status: :created
+        else
+          render json: post.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def set_post
+        @post = Post.find(params[:id])
+      end
+
+      def post_params
+        params.required(:post).permit(:title, :body, :status)
       end
     end
   end
