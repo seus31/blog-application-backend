215b928dbd5afbc0a403444a797ab19c130ac1d4
seus31 <us.balut.big.dipper@gmail.com>
2025-02-09 22:55:07 +0900

    ユーザーテーブルのマイグレーション作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_06_153726) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_09_061941) do
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "category_name"
     t.datetime "created_at", null: false
@@ -21,6 +21,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_06_153726) do
     t.string "title"
     t.text "body"
     t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
